317f990973b685ad1b8b2bf120524385d4f813e4
Jon Stafford <staffordjon@gmail.com>
2016-11-04 17:26:15 -0600

    completed bubble sort

--- bubble/lib/bubble.rb
+++ bubble/lib/bubble.rb
@@ -1,4 +1,4 @@
-def bubble_sort arr
+def bubble_sort_by(arr, &comparator)
   
   pairs_count = arr.length - 1
   
@@ -6,11 +6,15 @@ def bubble_sort arr
     return arr
   else
     pairs_count.times do |i|
-      if (arr[i] > arr[i+1])
+      if (comparator.call(arr[i],arr[i+1]) > 0)
         arr[i], arr[i+1] = arr[i+1], arr[i]
       end
     end
   end
   
-  return bubble_sort(arr[0..-2]) + [arr[-1]]
+  return bubble_sort_by(arr[0..-2], &comparator) + [arr[-1]]
+end
+
+def bubble_sort arr
+  return bubble_sort_by(arr, &Proc.new { |left, right| left - right })
 end
--- bubble/tests/test_bubble.rb
+++ bubble/tests/test_bubble.rb
@@ -4,8 +4,13 @@ require "test/unit"
 class TestBubble < Test::Unit::TestCase
 
   def test_bubble_sort
-    
     assert_equal([0,2,2,3,4,78], bubble_sort([4,3,78,2,0,2]))
+  end
+  
+  def test_bubble_sort_by
+    assert_equal(["hi", "hey", "hello"], bubble_sort_by(["hi","hello","hey"], &Proc.new do |left, right|
+      left.length - right.length 
+    end))
   end
 
 end
