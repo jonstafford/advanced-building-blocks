6a0000868f70017737fd45c5ddef7cc37d008c30
Jon Stafford <staffordjon@gmail.com>
2016-11-07 10:18:52 -0700

    solutions for enumerable methods

--- enumerable/lib/enumerable.rb
+++ enumerable/lib/enumerable.rb
@@ -27,4 +27,73 @@ module Enumerable
     result
   end
   
+  # do all my elements answer true for the block
+  def my_all?
+    my_each do |i|
+      if (!yield(i))
+        return false
+      end
+    end
+    true
+  end
+  
+  def my_any?
+    my_each do |i|
+      if (yield(i))
+        return true
+      end
+    end
+    false
+  end
+  
+  def my_none? &block
+    !my_any?(&block)
+  end
+  
+  def my_count
+    result = 0
+    my_each do |i|
+      if (yield(i))
+        result += 1
+      end
+    end
+    result
+  end
+  
+  def my_map &the_proc
+    result = []
+    unless (the_proc.nil?)
+      my_each do |i|
+        result << yield(i)
+      end
+    else
+      my_each do |i|
+        result << the_proc.call(i)
+      end
+    end
+    result
+  end
+  
+  def my_inject (initial=0)
+    if (initial.is_a? Symbol)
+      result = first
+      is_first = true
+      self.my_each do |i|
+        if (is_first)
+          is_first = false
+        else
+          result = result.method(initial).call(i)
+        end
+      end
+      result
+    else
+      result = initial
+      self.my_each do |i|
+        result = yield(result, i)
+      end
+      result
+    end
+  end
+  
+  
 end
--- enumerable/tests/test_enumerable.rb
+++ enumerable/tests/test_enumerable.rb
@@ -21,5 +21,72 @@ class TestEnumerable < Test::Unit::TestCase
     arr = Array.new [1, 2, 3, 4]
     assert_equal([2, 4], arr.my_select { |num| num % 2 == 0 })
   end
-
+  
+  def test_my_all_true
+    arr = Array.new [2, 2, 8, 4]
+    assert_equal(true, arr.my_all? { |num| num % 2 == 0 })
+  end    
+  
+  def test_my_all_false
+    arr = [3, 3, 1, 3]
+    assert_equal(false, arr.my_all? { |num| num  == 3 })
+  end   
+  
+  def test_my_any_true
+    arr = [3, 3, 1, 3]
+    assert_equal(true, arr.my_any? { |num| num  == 1 })
+  end   
+   
+  def test_my_any_false
+    arr = [3, 3, 1, 3]
+    assert_equal(false, arr.my_any? { |num| num % 2 == 0 })
+  end  
+   
+  def test_my_none_true
+    arr = [3, 3, 5, 3]
+    assert_equal(true, arr.my_none? { |num| num % 2 == 0 })
+  end   
+  
+  def test_my_none_false
+    arr = [3, 3, 2, 3]
+    assert_equal(false, arr.my_none? { |num| num % 2 == 0 })
+  end   
+  
+  def test_my_count
+    arr = [3, 3, 2, 3]
+    assert_equal(1, arr.my_count { |num| num % 2 == 0 })
+  end   
+  
+  def test_my_map
+    arr = [4,5,7,8]
+    assert_equal([8,10,14,16], arr.my_map { |num| num * 2  } )
+  end   
+  
+  def test_my_map_with_proc
+    arr = [4,5,7,8]
+    the_proc = Proc.new { |num| num * 2  }
+    assert_equal([8,10,14,16], arr.my_map(&the_proc))
+  end   
+  
+  
+  def test_my_inject_symbol
+    assert_equal(45, [5,6,7,8,9,10].my_inject(:+))
+  end
+  
+  def test_my_inject_just_block
+    assert_equal(45, [5,6,7,8,9,10].my_inject { |sum, n| sum + n } )
+  end
+  
+  def test_my_inject_arg_and_block
+    assert_equal(151200, [5,6,7,8,9,10].my_inject(1) { |product, n| product * n })
+  end
+  
+  def multiply_els arr
+    arr.my_inject :*
+  end
+  
+  def test_multiply_els_to_test_my_inject
+    assert_equal(40, multiply_els([2,4,5]))
+  end
+  
 end
